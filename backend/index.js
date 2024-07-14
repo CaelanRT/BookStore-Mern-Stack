5c7ebdabdcb88019711c102a4dcd59ac9cec25cc
CaelanRT <93503205+CaelanRT@users.noreply.github.com>
2024-07-14 00:00:13 -0400

    adding more funcitonality in the backend, up to lesson 9

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
-import express from "express";
+import express, { request } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose';
 import { Book } from './models/bookModel.js';
 
 const app = express();
+
+// Middlewar for parsing request body
+app.use(express.json());
 
 app.get('/', (request, response) => {
     console.log(request);
@@ -35,6 +38,63 @@ app.post('/books', async (request, response) => {
         response.status(500).send({message: error.message});
     }
 });
+
+// Route for Get All Books from Database
+app.get('/books', async (request, response) => {
+    try {
+        const books = await Book.find({});
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+// Route for Get One Books from Database by id
+app.get('/books/:id', async (request, response) => {
+    try {
+        
+        const { id } = request.params;
+        
+        const book = await Book.findById(id);
+
+        return response.status(200).json(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+// Route for Updating a Book
+app.put('/books/:id', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {return response.status(400).send({message: 'Send all required fields: title, author, publishYear'});
+    }
+
+    const { id } = request.params;
+
+    const result = await Book.findByIdAndUpdate(id, request.body);
+
+    if (!result) {
+        return response.status(404).json({message: 'Book not found'});
+    }
+
+    return response.status(200).send({message: 'Book updated successfully'});
+        
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+        
+    }
+});
+
 
 mongoose.connect(mongoDBURL).then(() => {
     console.log('App connected to database');
