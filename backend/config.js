ffadb4000bdec0ae1223900bb25f71f2410bdd2a
CaelanRT <93503205+CaelanRT@users.noreply.github.com>
2024-07-13 22:42:16 -0400

    first commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://caelanrt:Squidge007!@books-store-mern.naeg4jv.mongodb.net/?retryWrites=true&w=majority&appName=Books-Store-MERN';
